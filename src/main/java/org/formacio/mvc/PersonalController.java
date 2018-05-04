3dd967a2b0090f00d2ac072b85e9ecc58244f627
[adriangalende] <[adriangalende@gmail.com]>
2018-05-04 18:11:10 +0200

    test_afegir

--- src/main/java/org/formacio/mvc/PersonalController.java
+++ src/main/java/org/formacio/mvc/PersonalController.java
@@ -1,11 +1,25 @@
 package org.formacio.mvc;
 
+import ch.qos.logback.core.status.ErrorStatus;
+import ch.qos.logback.core.status.Status;
+import com.sun.deploy.net.HttpResponse;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
+import java.net.StandardProtocolFamily;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.ResponseEntity.status;
 
 
 @Controller
@@ -43,5 +57,15 @@ public class PersonalController {
 		@ResponseBody
 		public String consultaPathVariable(@PathVariable  int id){
 			return getBaseDeDades().get(id);
+		}
+
+		@RequestMapping(path="/afegir", method=RequestMethod.POST)
+		@ResponseBody
+		public ResponseEntity<String> addAlumno(@RequestParam(name="nom") String nom){
+			if (nom.split(",").length > 1){
+				return new ResponseEntity<>("error", HttpStatus.UNAUTHORIZED);
+			}
+			getBaseDeDades().add(nom);
+			return new ResponseEntity<>("ok", HttpStatus.OK) ;
 		}
 }
